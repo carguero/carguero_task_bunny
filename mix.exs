905cfd9de040972aa0950f7a4067685ce4903e3d
lfmalves <luis.projensino@gmail.com>
2020-12-03 10:10:23 -0300

    naming

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule TaskBunny.Mixfile do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.0.3"
   @description "Background processing application/library written in Elixir that uses RabbitMQ as a messaging backend"
 
   def project do
